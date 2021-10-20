3a32cba7e0b67aa0363cec60ba84b4f7f47f3685
mariam.k <mariamkarapetyan1401@gmail.com>
2021-10-20 18:09:16 +0400

    added new examples

--- symbolmatch.js
+++ symbolmatch.js
@@ -1,4 +1,4 @@
-let a = "()()";
+/*let a = "()()";
 let b = "()(";
 let arr1 = [];
 let arr2 = [];
@@ -19,7 +19,7 @@ function checkSymbol(val){
     }
 }
 
-done(a);
+done(a);*/
 
 /*let arr3 = [];
 function checkSymbol(val){
@@ -39,3 +39,32 @@ function checkSymbol(val){
     }
     }
 checkSymbol("(()())");*/
+
+/*Write a function named calculateDogAge that:
+    takes 1 argument: your puppy's age.
+calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+Call the function three times with different sets of values.*/
+
+
+function calculateDogAge (age) {
+    let dogAge = 7 * age
+   console.log(dogAge);
+ }
+calculateDogAge(5);
+calculateDogAge(30);
+calculateDogAge(1);
+
+function tellFortune(jobTitle, geoLocation, partner, numKids) {
+    var future = "You will be a " + jobTitle + " in " + geoLocation + " and married to " +
+        partner + " " + " with " + numKids + " kids.";
+    console.log(future);
+}
+
+tellFortune('Manager', 'Armenia', 'David', 3);
+tellFortune('Developer', 'America', 'Joe', 1);
+tellFortune('Singer', 'Russia', 'Nike', 0);
+
+
+
+
